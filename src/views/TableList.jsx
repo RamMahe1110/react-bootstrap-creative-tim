c9a85f5533310f3ee87e2bfcb3ea9fd1109bccdf
Ram Maheshwari <ram991011@gmail.com>
2020-09-08 11:06:26 +0530

    first commit

--- src/views/TableList.jsx
+++ src/views/TableList.jsx
@@ -15,16 +15,82 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import React, { Component } from "react";
-import { Grid, Row, Col, Table } from "react-bootstrap";
-
-import Card from "components/Card/Card.jsx";
-import { thArray, tdArray } from "variables/Variables.jsx";
+import React, { Component } from 'react'
+import { Grid, Row, Col, Table } from 'react-bootstrap'
+import Card from 'components/Card/Card.jsx'
+import { thArray, tdArray } from 'variables/Variables.jsx'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import axios from 'axios'
+import '../assets/sass/TableList.scss'
+import Loader from 'react-loader-spinner'
 
 class TableList extends Component {
+  state = {
+    users: [],
+    allowedValues: {
+      _id: 'Id',
+      firstName: 'First Name',
+      email: 'Email',
+    },
+    currEdit: null,
+    editName: '',
+    load: false,
+    mainLoader: true,
+  }
+
+  componentDidMount() {
+    let getUrl = 'http://18.188.185.178:3002/get/candidate'
+    axios.get(getUrl).then((res) => {
+      this.setState({
+        users: res.data.data,
+        mainLoader: false,
+      })
+    })
+  }
+
+  setCurrentEdit = (id, name) => {
+    if (this.state.load) return
+    this.setState({ currEdit: id, editName: name })
+  }
+
+  onNameEdit = (e) => {
+    this.setState({ editName: e.target.value })
+  }
+
+  onUpdate = () => {
+    const { currEdit, editName, users } = this.state
+    if (!editName.length) return toast.error("Name can't be empty")
+    let putUrl = 'http://18.188.185.178:3002/put/candidate'
+    this.setState({
+      load: true,
+    })
+    axios
+      .put(putUrl, {
+        id: currEdit,
+        set: {
+          firstName: editName,
+        },
+      })
+      .then((res) => {
+        let userId = users.findIndex((item) => item._id === currEdit)
+        users[userId] = res.data.data
+        this.setState({ users, currEdit: null, editName: '', load: false })
+      })
+  }
+
   render() {
+    const {
+      users,
+      allowedValues,
+      currEdit,
+      editName,
+      load,
+      mainLoader,
+    } = this.state
     return (
       <div className="content">
+        <ToastContainer type="warning" />
         <Grid fluid>
           <Row>
             <Col md={12}>
@@ -37,22 +103,87 @@ class TableList extends Component {
                   <Table striped hover>
                     <thead>
                       <tr>
-                        {thArray.map((prop, key) => {
-                          return <th key={key}>{prop}</th>;
-                        })}
+                        {Object.keys(allowedValues).map((key, idx) => (
+                          <th key={key}>{allowedValues[key]}</th>
+                        ))}
                       </tr>
                     </thead>
-                    <tbody>
-                      {tdArray.map((prop, key) => {
-                        return (
-                          <tr key={key}>
-                            {prop.map((prop, key) => {
-                              return <td key={key}>{prop}</td>;
+                    {mainLoader ? (
+                      <div className="loading-box">
+                        <Loader
+                          type="Oval"
+                          color="rgb(200,200,200)"
+                          height={70}
+                          width={70}
+                          className="main-loader"
+                        />
+                      </div>
+                    ) : (
+                      <tbody>
+                        {users.map((item, index) => (
+                          <tr key={index}>
+                            {Object.keys(allowedValues).map((key, idx) => {
+                              if (allowedValues[key]) {
+                                return currEdit === item._id &&
+                                  key === 'firstName' ? (
+                                  <td key={idx}>
+                                    <input
+                                      value={editName}
+                                      onChange={this.onNameEdit}
+                                    />
+                                  </td>
+                                ) : (
+                                  <td
+                                    style={{
+                                      width: key === '_id' ? '10%' : '30%',
+                                    }}
+                                    key={idx}
+                                  >
+                                    {key === '_id'
+                                      ? index + 1
+                                      : item[key]
+                                      ? item[key]
+                                      : 'NOT PRESENT'}
+                                  </td>
+                                )
+                              } else {
+                                return null
+                              }
                             })}
+                            {currEdit === item._id ? (
+                              <td className="tableFieldCont">
+                                {load ? (
+                                  <button className="btn btn-fill btn-success lg t-btn">
+                                    updating...
+                                  </button>
+                                ) : (
+                                  <button
+                                    className="btn btn-fill btn-success lg t-btn"
+                                    onClick={this.onUpdate}
+                                  >
+                                    Update
+                                  </button>
+                                )}
+                              </td>
+                            ) : (
+                              <td className="tableFieldCont">
+                                <button
+                                  className="btn btn-fill btn-info lg t-btn"
+                                  onClick={() =>
+                                    this.setCurrentEdit(
+                                      item._id,
+                                      item.firstName
+                                    )
+                                  }
+                                >
+                                  Edit
+                                </button>
+                              </td>
+                            )}
                           </tr>
-                        );
-                      })}
-                    </tbody>
+                        ))}
+                      </tbody>
+                    )}
                   </Table>
                 }
               />
@@ -70,7 +201,7 @@ class TableList extends Component {
                     <thead>
                       <tr>
                         {thArray.map((prop, key) => {
-                          return <th key={key}>{prop}</th>;
+                          return <th key={key}>{prop}</th>
                         })}
                       </tr>
                     </thead>
@@ -79,10 +210,10 @@ class TableList extends Component {
                         return (
                           <tr key={key}>
                             {prop.map((prop, key) => {
-                              return <td key={key}>{prop}</td>;
+                              return <td key={key}>{prop}</td>
                             })}
                           </tr>
-                        );
+                        )
                       })}
                     </tbody>
                   </Table>
@@ -92,8 +223,8 @@ class TableList extends Component {
           </Row>
         </Grid>
       </div>
-    );
+    )
   }
 }
 
-export default TableList;
+export default TableList
